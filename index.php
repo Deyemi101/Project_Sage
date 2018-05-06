a9478d91968fc2231024d3c79d657c4522e3a4cc
Deyemi <ayokunmiajala@gmail.com>
2018-05-06 16:31:54 +0100

    Initial Trial at Push

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/sage.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="css/bootstrap.min.css"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <title>Project Sage</title>
+</head>
+
+<body style="background-color:#6d8b4d;">
+    <div class="container">
+        <div class="content-main">
+            <div class="row">
+                <div class="col-sm-6"><br/>
+                    <img src="img/sage.jpg"  class="img-responsive" alt="observing frog">
+                </div>
+                <div class="col-sm-6" style="text-align: center;">
+                    <h1 id="h01">HNG INTERNSHIP 4.0</h1>
+                    <H3>PROVIDES</H3>
+                    <h4>MENTORSHIP FOR WHO</h4>
+                    <textarea rows="4" cols="30">What troubles you?</textarea><br/>
+                    <button type="button" class="btn" id="btn-seek">SEEKS</button><br/>
+                    <span id="date">DATE: <?php echo date("d.m.Y");?></span>
+                    <span id="time">TIME: <?php echo date("H:i:sa");?></span>
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
